84d86c9ceac74436142000bf60944f8f874041ca
Gabor Szarnyas <szarnyasg@gmail.com>
2015-11-14 16:52:45 +0100

    Renames

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-svgexport circle1.svg circle1.png 
-svgexport circle2.svg circle2.png 
+svgexport circle1.svg circle1-1x.png 
+svgexport circle2.svg circle2-1x.png 
 
-svgexport circle1.svg 2x-circle1.png 2x
-svgexport circle2.svg 2x-circle2.png 2x
+svgexport circle1.svg circle1-2x.png 2x
+svgexport circle2.svg circle2-2x.png 2x
